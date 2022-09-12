2e05afc0b3bef0982fa932e8a7b6b914f23e4a3f
Ashray._.g <ashray.gupta@gmail.com>
2022-09-12 00:38:30 -0700

    fixed initial joint calculation + commented

--- main.cpp
+++ main.cpp
@@ -11,13 +11,18 @@ typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> MatrixXd;
 
 int main() {
 
-    vector<vector<float>> DHTable = {{1, 0, 0, 0},
-                                     {1, 0, 0, 3.14/6},
-                                     {1, 0, 0, 3.14/6},
-                                     {1, 0, 0, 3.14/6}};
+    //Physical constants
+    vector<float> linkLengths = {1, 1, 1, 1};
+    vector<float> jointAngles = {0, 30, 30, 30};
 
+    // Denavit-Hartenberg Parameters
+    vector<vector<float>> DHTable = {{linkLengths[0], 0, 0, jointAngles[0] * 3.1415f / 180},
+                                     {linkLengths[1], 0, 0, jointAngles[1] * 3.1415f / 180},
+                                     {linkLengths[2], 0, 0, jointAngles[2] * 3.1415f / 180},
+                                     {linkLengths[3], 0, 0, jointAngles[3] * 3.1415f / 180}};
+
+    // Homogeneous transform matrix for each link
     vector<MatrixXd> links = {};
-
 
     for(int i = 0;i<DHTable.size();i++){
         links.emplace_back(4, 4);
@@ -31,24 +36,32 @@ int main() {
                 0, 0, 0, 1;
     }
 
+
+    // Find Pose3d of each join with forward-kinematics
     MatrixXd mult = links[0];
 
     vector<Pose3d> jointLocations;
 
+    MathUtils mathUtil;
+    vector<double> rotation = mathUtil.rotationMatrixToEulerAngles(mult);
+
+    jointLocations.push_back(*new Pose3d(mult(0, 3), mult(1, 3), mult(2, 3), rotation[0], rotation[1], rotation[2]));
+
+
     for(int i = 1;i<DHTable.size();i++){
         mult = mult * links[i];
 
-        MathUtils mathUtil;
-        vector<double> rotation = mathUtil.rotationMatrixToEulerAngles(mult);
+        rotation = mathUtil.rotationMatrixToEulerAngles(mult);
 
         jointLocations.push_back(*new Pose3d(mult(0, 3), mult(1, 3), mult(2, 3), rotation[0], rotation[1], rotation[2]));
     }
 
+    // Output
     int i = 0;
     for(Pose3d pose : jointLocations){
         i++;
-        cout << "Joint " << i << ": \n" << "POSE x: " << pose.getX() <<  "   y: " << pose.getY() << "   z: " << pose.getZ() << "\n";
-        cout << "ORIENTATION Around x: " << pose.getRotX() << " Around y: " << pose.getRotY() << " Around z: " << pose.getRotZ() << "\n\n";
+        cout << "Joint " << i << ": \n" << "[POSE] x: " << pose.getX() <<  " | y: " << pose.getY() << " | z: " << pose.getZ() << "\n";
+        cout << "[ORIENTATION] Around x: " << pose.getRotX() << " | Around y: " << pose.getRotY() << " | Around z: " << pose.getRotZ() << "\n\n";
     }
 
     return 0;
